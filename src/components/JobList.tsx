67c58cd0f942a5a3b1ff0f745e5506507f87687c
Yugank Singh <yuganksingh05@gmail.com>
2024-05-06 06:52:00 +0530

    add the jobs to the JobList

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,13 +1,39 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useAppSelector } from "../hooks"
+import JobPostingCard from "./JobPostingCard"
+import { Grid } from "@mui/material"
+import LazyLoad from "react-lazy-load"
 
 interface JobsListProps {}
 
 function JobsList({}: JobsListProps) {
 	const jobs = useAppSelector(state => state.jobs.list)
+	const onJobCardVisible = (index: number) => {
+		console.log(index, "visible")
+	}
+
+	if (!jobs)
+		return <h1>Loading...</h1>
+
 	return (
 		<div>
-			<pre>{JSON.stringify(jobs, null, 2)}</pre>
+			<Grid container spacing={4}>
+				{jobs.map((job, index) => (
+					<Grid
+						key={index}
+						item
+						xs={12}
+						sm={6}
+						md={4}
+						sx={{ py: "30px", px: "15px" }}
+					>
+						<JobPostingCard
+							job={job}
+							// onJobCardVisible={() => onJobCardVisible(index)}
+						/>
+					</Grid>
+				))}
+			</Grid>
 		</div>
 	)
 }
