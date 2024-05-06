5cb5cc48e5eb7f2839be6c095cc126f4751cb636
Yugank Singh <yuganksingh05@gmail.com>
2024-05-06 06:50:44 +0530

    make changes in jobsSlice

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import "./App.css"
 import JobsList from "./components/JobList"
 import JobFiltersList from "./components/JobFiltersList"
 import { Container } from "@mui/material"
+import { useEffect } from "react"
+import { fetchMoreJobs } from "./store/jobsSlice"
+import { useAppDispatch } from "./hooks"
 
 function App() {
+	const dispatch = useAppDispatch()
+	useEffect(() => {
+		dispatch(fetchMoreJobs())
+	}, [])
+
 	return (
 		<div className="App">
 			<Container maxWidth="lg">
 				<JobFiltersList />
 				<JobsList />
-			</Container >
+			</Container>
+			<footer
+				style={{ display: "block", height: "300px",  }}
+			></footer>
 		</div>
 	)
 }
--- src/store/jobsSlice.ts
+++ src/store/jobsSlice.ts
@@ -1,8 +1,69 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { Job } from "../types"
 
-const initialJobsState = {
-	list: [{ name: "First Job", description: "This is the job description" }],
-	areAllLoaded: false,
+interface JobsState {
+	list: Job[]
+	status: "idle" | "loading" | "succeeded" | "failed"
+	error: string | null
+	totalCount: number
+}
+
+const fetchJobs = async (limit: number, offset: number) => {
+	const myHeaders = new Headers()
+	myHeaders.append("Content-Type", "application/json")
+
+	const body = JSON.stringify({
+		limit,
+		offset,
+	})
+
+	const requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body,
+	}
+
+	const response = await fetch(
+		"https://api.weekday.technology/adhoc/getSampleJdJSON",
+		requestOptions
+	)
+	if (!response.ok) {
+		throw new Error("Failed to fetch data")
+	}
+	const data = await response.json()
+	return data
+}
+
+export const fetchMoreJobs = createAsyncThunk(
+	"jobs/fetchMoreJobs",
+	async (data, thunkAPI) => {
+		try {
+			const fetchedData = (await fetchJobs(10, 0)) as any
+			console.log(fetchedData)
+			let jdList = fetchedData.jdList as Job[]
+			jdList = jdList.map(jd => {
+				if (jd.salaryCurrencyCode === "USD") {
+					return {
+						...jd,
+						salaryCurrencyCode: "INR",
+						maxJdSalary: jd.maxJdSalary ? jd.maxJdSalary * 0.8338 : null,
+						minJdSalary: jd.minJdSalary ? jd.minJdSalary * 0.8338 : null,
+					}
+				}
+				return jd
+			})
+			return { list: jdList, totalCount: fetchedData.totalCount } // Return list and totalCount
+		} catch (error: any) {
+			return thunkAPI.rejectWithValue(error.message)
+		}
+	}
+)
+
+const initialJobsState: JobsState = {
+	list: [],
+	status: "idle",
+	error: null,
+	totalCount: 1000,
 }
 
 const jobsSlice = createSlice({
@@ -10,6 +71,21 @@ const jobsSlice = createSlice({
 	initialState: initialJobsState,
 	reducers: {
 		addJobs: state => {},
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(fetchMoreJobs.pending, state => {
+				state.status = "loading"
+			})
+			.addCase(fetchMoreJobs.fulfilled, (state, action) => {
+				state.status = "succeeded"
+				state.list = action.payload.list
+				state.totalCount = action.payload.totalCount
+			})
+			.addCase(fetchMoreJobs.rejected, (state, action) => {
+				state.status = "failed"
+				state.error = action.error.message || null
+			})
 	},
 })
 
