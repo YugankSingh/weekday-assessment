9c6c238c112f6210b56b28a307b9a7a4f4debb54
Yugank Singh <yuganksingh05@gmail.com>
2024-05-06 19:25:34 +0530

    include type for Filters

--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,13 @@ export interface Job {
 	companyName: string
 	logoUrl: string
 }
+
+export interface Filters {
+	roles: string[]
+	techStack: string[]
+	minExperience: number | null
+	companyName: string
+	location: string[]
+	workEnvironment: ("Hybrid" | "In-Office" | "Remote")[]
+	minBasePayInLPA: number | null
+}
