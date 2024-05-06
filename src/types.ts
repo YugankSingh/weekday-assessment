958d06670b39d57c18b909b810ba44c7f338baed
Yugank Singh <yuganksingh05@gmail.com>
2024-05-06 06:51:38 +0530

    add Job Type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,18 @@
 export interface jobFilterGroupedValues {
 	[groupName: string]: string[]
 }
+
+export interface Job {
+	jdUid: string
+	jdLink: string
+	jobDetailsFromCompany: string
+	maxJdSalary: number | null
+	minJdSalary: number | null
+	salaryCurrencyCode: string
+	location: string
+	minExp: number
+	maxExp: number
+	jobRole: string
+	companyName: string
+	logoUrl: string
+}
